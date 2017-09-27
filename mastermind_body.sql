41cf3cc103f612ad9c8454ab489534acf4eb7e5e
Paul Muller <pmuller@iqn.com>
2017-09-27 14:05:13 -0400

    broke package file into spec and body files

--- mastermind_body.sql
+++ mastermind_body.sql
@@ -1,17 +1,3 @@
-CREATE OR REPLACE PACKAGE mastermind
-AS
-  TYPE combination IS VARRAY(4) OF integer;
-
-  FUNCTION combination_to_string(fi_combo combination) RETURN VARCHAR2;
-
-  PROCEDURE compute_score (pi_guess       IN  combination,
-                           pi_solution    IN  combination,
-                           po_white_count OUT NUMBER,
-                           po_black_count OUT NUMBER);
-
-END mastermind;
-/
-
 CREATE OR REPLACE PACKAGE BODY mastermind
 AS
 
